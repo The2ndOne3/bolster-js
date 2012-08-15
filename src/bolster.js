31b747303713ba338e89af942ea4d3db8f0f6e35
The2ndOne3 <rydenfreak@gmail.com>
2012-08-14 23:24:59 -0600

    JSHint'd

--- src/bolster.js
+++ src/bolster.js
@@ -1,7 +1,11 @@
+/*jshint mootools:true,bitwise:true,curly:true,eqeqeq:true,noarg:true,undef:true,unused:true,strict:true,trailing:true */
+
 /**
  * Bolstering CSS with dynamic effects, in the most literal sense possible.
  */
-!function(mT){
+(function(mT){
+	"use strict";
+	
 	// Prerequisites.
 	if(mT===undefined||mT.version.toFloat()<1.4){
 		console.log('Bolster requires at least MooTools 1.4.5');
@@ -11,213 +15,215 @@
 		console.log('Bolster requires at least MooTools More 1.2.5');
 		return;
 	}
+	
 	// Initialize plugin.
 	window.Bolster={
-		version:'', // Version.
+		version:'0.1.0', // Version.
 		extend:function(property_name,func){ // Add custom properties.
-			bolsterfx[property_name]=func;
+			this.bolsterfx[property_name]=func;
 		}
 	};
+	// Initialize temporary object.
+	this.temp={};
+	
 	// Initialize CSS input object.
-	input={
+	this.input={
 		count:$(document.head).getChildren('link[type="text/css"]').length, // The number of files
 		loaded:0, // The counter of loaded files.
 		text:''
-	};	
+	};
 	// Get inline CSS.
 	$$('style[type="text/css"]').each(function(style){
-		input.text+=style.get('html');
+		this.temp.input.text+=style.get('html');
 	});
 	// Get the CSS files.
 	$(document.head).getChildren('link[type="text/css"]').each(function(link){
 		new Request({
 			url:link.get('href'),
-			onSuccess:function(response){
-				input.text+=response;
-				input.loaded++;
-				if(input.loaded===input.count){
-					parse(input.text);
+			onSuccess:(function(response){
+				this.input.text+=response;
+				this.input.loaded++;
+				if(this.input.loaded===this.input.count){
+					this.parse(this.input.text);
 				}
-			}
+			}).bind(this)
 		}).send();
-	});
+	},this);
+	
 	// Parse for Bolster keywords.
-	function parse(css){
+	this.parse=(function(css){
 		// Parse for CSS declarations.
 		css=css.split(/[{}]/g);
 		css.pop(); // Remove last item; will be the 'empty' text after the last '}'.
-		rules=[];
-		for(i=0;i<css.length/2;i++){
-			rules.push({
-				selector:css[2*i].trim().replace(/[\t\n]/g,''),
-				'css':css[2*i+1],
+		this.rules=[];
+		for(this.temp.i=0;this.temp.i<css.length/2;this.temp.i++){
+			this.rules.push({
+				selector:css[2*this.temp.i].trim().replace(/[\t\n]/g,''),
+				'css':css[2*this.temp.i+1],
 				declarations:[]
 			});
 		}
 		// Parse CSS into rules.
-		rules.each(function(rule){
-			declarations=rule.css.split(/;/g);
-			declarations.pop(); // Same as above.
-			declarations.each(function(declaration){
+		this.rules.each((function(rule){
+			this.temp.declarations=rule.css.split(/;/g);
+			this.temp.declarations.pop(); // Same as above.
+			this.temp.declarations.each(function(declaration){
 				declaration=declaration.split(/:/g);
 				rule.declarations.push({
 					property:declaration[0].trim().replace(/[\t\n]/g,''),
 					value:declaration[1].trim().replace(/[\t\n]/g,'')
 				});
 			});
-		});
+		}).bind(this));
 		// Parse rules for Bolster properties.
-		rules.each(function(rule){
-			rule.declarations.each(function(declaration){
-				get_effects().each(function(bolster_property){
+		this.rules.each((function(rule){
+			rule.declarations.each((function(declaration){
+				this.get_effects().each((function(bolster_property){
 					if(declaration.property==='-bolster-'+bolster_property){
 						// Format whitespace.
-						value=declaration.value.replace(/ +(?= )/g,''); // Remove multiple spaces.
-						value=declaration.value.replace(/, /g,','); // Remove space after comma.
+						this.temp.value=declaration.value.replace(/ +(?= )/g,''); // Remove multiple spaces.
+						this.temp.value=declaration.value.replace(/, /g,','); // Remove space after comma.
 						// Tokenize.
-						args=[];
-						last=0;
-						quoted=false;
+						this.temp.args=[];
+						this.temp.last=0;
+						this.temp.quoted=false;
 						//escape=false;
-						temp=[];
-						for(i=0,len=value.length;i<len;i++){
-							switch(value[i]){
+						this.temp.bucket=[];
+						for(this.temp.i=0,this.temp.len=this.temp.value.length;this.temp.i<this.temp.len;this.temp.i++){
+							switch(this.temp.value[this.temp.i]){
 								case "'":
 								case '"':
-									if(quoted===false){
-										quoted=value[i];
-										last++; // So it won't match the leading quotation mark.
-									}else if(quoted===value[i]){
-										temp.push(value.substring(last,i));
-										last=i+1;
-										quoted=false;
+									if(this.temp.quoted===false){
+										this.temp.quoted=this.temp.value[this.temp.i];
+										this.temp.last++; // So it won't match the leading quotation mark.
+									}else if(this.temp.quoted===this.temp.value[this.temp.i]){
+										this.temp.bucket.push(this.temp.value.substring(this.temp.last,this.temp.i));
+										this.temp.last=this.temp.i+1;
+										this.temp.quoted=false;
 									}
 									break;
 								case ' ':
-									if(quoted===false){
-										temp.push(value.substring(last,i));
-										last=i+1;
+									if(this.temp.quoted===false){
+										this.temp.bucket.push(this.temp.value.substring(this.temp.last,this.temp.i));
+										this.temp.last=this.temp.i+1;
 									}
 									break;
 								case ',':
-									if(quoted===false){
-										temp.push(value.substring(last,i));
-										last=i+1;
-										args.push(temp);
-										temp=[];
+									if(this.temp.quoted===false){
+										this.temp.bucket.push(this.temp.value.substring(this.temp.last,this.temp.i));
+										this.temp.last=this.temp.i+1;
+										this.temp.args.push(this.temp.bucket);
+										this.temp.bucket=[];
 									}
 									break;
 							}
 						}
 						// The last token won't have a trailing comma or space.
-						temp.push(value.substring(last));
-						args.push(temp);
+						this.temp.bucket.push(this.temp.value.substring(this.temp.last));
+						this.temp.args.push(this.temp.bucket);
 						// Clean arguments.
-						cleaned=[];
-						args.each(function(tokens){
+						this.temp.cleaned=[];
+						this.temp.args.each((function(tokens){
 							// Remove invalid tokens.
 							tokens=tokens.clean();
 							// Remove empty tokens.
-							temp=[]
-							tokens.each(function(token){
+							this.temp.nonempty=[];
+							tokens.each((function(token){
 								if(token!==''){
-									temp.push(token);
+									this.temp.nonempty.push(token);
 								}
-							});
+							}).bind(this));
 							// If only one token, push that instead of the array.
-							if(temp.length<=1){
-								cleaned.push(temp[0]);
+							if(this.temp.nonempty.length<=1){
+								this.temp.cleaned.push(this.temp.nonempty[0]);
 							}
 							else{
-								cleaned.push(temp);
+								this.temp.cleaned.push(this.temp.nonempty);
 							}
-						});
-						if(cleaned.length<=1){
-							cleaned=cleaned[0];
+						}).bind(this));
+						if(this.temp.cleaned.length<=1){
+							this.temp.cleaned=this.temp.cleaned[0];
 						}
-						set_effect(rule.selector,bolster_property,cleaned);
-					}					
-				});
-			});
-		});
-	}
+						this.set_effect(rule.selector,bolster_property,this.temp.cleaned);
+					}
+				}).bind(this));
+			}).bind(this));
+		}).bind(this));
+	}).bind(this);
+	
 	// Get effect names.
-	function get_effects(){
-		return Object.keys(bolsterfx);
-	}
+	this.get_effects=function(){
+		return Object.keys(this.bolsterfx);
+	};
 	// Effect setter.
-	function set_effect(selector,property,args){
-		bolsterfx[property](selector,args);
-	}
+	this.set_effect=function(selector,property,args){
+		this.bolsterfx[property](selector,args);
+	};
 	// Effect configuration.
-	bolsterfx={
+	this.bolsterfx={
 		'test-property':function(selector,args){
 			console.log('Selector: '+selector+';');// Declaration:"test-property:'+args+';"');
 			console.log(args);
 		},
 		// Vertically centre dynamic content
-		'vert-mount':function(selector,args){
-			$$(selector).each(function(element){
-				if(value===''){
-					;
-				}
-			});
-		},
+		/*'vert-mount':function(selector,args){
+			;
+		},*/
 		// Natural height given in absolute pixels
 		'abs-height':function(selector,arg){
-			if(arg=='auto'){
-				add_new_continuous(function(){
+			if(arg==='auto'){
+				this.add_new_continuous(function(){
 					$$('selector').each(function(element){
-						element.setStyle('height',element.getSize().y);
-					})
+						element.setStyle('width',element.getSize().x);
+					});
 				});
 			}
 			else{ // If they're an idiot and decide to use this for a regular height declaration.
-				sheets_height:
-				for(i=0,lenA=document.styleSheets.length;i<len;i++){ // Traditional for loops must be used to use labels.
-					sheet=document.styleSheets.length[i];
-					for(j=0,lenB=sheet.cssRules.length;j<lenB;j++){
-						rule=document.styleSheets[i].cssRules[j];
-						if(rule.selectorText==selector){
-							rule.style.setProperty('height',arg);
-							break sheets_height;
-						}
-					}
-				}
-			}
-		},
-		// Natural width given in absolute pixels
-		'abs-width':function(selector,args){
-			if(arg=='auto'){
-				add_new_continuous(function(){
-					$$('selector').each(function(element){
-						element.setStyle('width',element.getSize().x);
-					})
-				});
-			}
-			else{
 				sheets_width:
-				for(i=0,lenA=document.styleSheets.length;i<len;i++){
-					sheet=document.styleSheets.length[i];
-					for(j=0,lenB=sheet.cssRules.length;j<lenB;j++){
-						rule=document.styleSheets[i].cssRules[j];
-						if(rule.selectorText==selector){
-							rule.style.setProperty('width',arg);
+				for(this.temp.i=0,this.temp.lenA=document.styleSheets.length;this.temp.i<this.temp.lenA;this.temp.i++){ // Traditional for loops must be used to use labels.
+					this.temp.sheet=document.styleSheets.length[this.temp.i];
+					for(this.temp.j=0,this.temp.lenB=this.temp.sheet.cssRules.length;this.temp.j<this.temp.lenB;this.temp.j++){
+						this.temp.rule=document.styleSheets[this.temp.i].cssRules[this.temp.j];
+						if(this.temp.rule.selectorText===selector){
+							this.temp.rule.style.setProperty('width',arg);
 							break sheets_width;
 						}
 					}
 				}
 			}
 		},
+		// Natural width given in absolute pixels
+		'abs-width':function(selector,arg){
+			if(arg==='auto'){
+				this.add_new_continuous(function(){
+					$$('selector').each(function(element){
+						element.setStyle('width',element.getSize().x);
+					});
+				});
+			}
+			else{
+				sheets_width:
+				for(this.temp.i=0,this.temp.lenA=document.styleSheets.length;this.temp.i<this.temp.lenA;this.temp.i++){
+					this.temp.sheet=document.styleSheets.length[this.temp.i];
+					for(this.temp.j=0,this.temp.lenB=this.temp.sheet.cssRules.length;this.temp.j<this.temp.lenB;this.temp.j++){
+						this.temp.rule=document.styleSheets[this.temp.i].cssRules[this.temp.j];
+						if(this.temp.rule.selectorText===selector){
+							this.temp.rule.style.setProperty('width',arg);
+							break sheets_width;
+						}
+					}
+				}
+			}
+		}
 	};
 	// Continuous functions.
-	continuous_check_functions=[];
-	function add_new_continuous(func){
-		continuous_check_functions.push(func);
+	this.continuous_check_functions=[];
+	this.add_new_continuous=function(func){
+		this.continuous_check_functions.push(func);
 	}
 	(function(){
-		continuous_check_functions.each(function(func){
-			func();
+		this.continuous_check_functions.each(function(func){
+			(func)();
 		});
-	}).periodical(1/32);
-}(window.MooTools);
+	}).bind(this).periodical(1/32);
+}).bind({})(window.MooTools);
