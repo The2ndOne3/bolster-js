a9cbf5e7f519140e1ffba072e893bc388664d92b
The2ndOne3 <rydenfreak@gmail.com>
2012-08-16 17:53:15 -0600

    I have no idea what I was doing. Properly scoped everything.

--- src/bolster.js
+++ src/bolster.js
@@ -20,18 +20,16 @@
 	window.Bolster={
 		version:'0.1.0', // Version.
 		extend:function(property_name,func){ // Add custom properties.
-			this.bolsterfx[property_name]=func;
-		}.bind(this),
+			bolsterfx[property_name]=func;
+		},
 		rescan:function(){
-			this.load();
-		}.bind(this)
+			load();
+		}
 	};
-	// Initialize temporary object.
-	this.temp={};
 
-	this.load=function(){
+	var load=function(){
 		// Initialize CSS input object.
-		this.input={
+		var input={
 			count:$(document.head).getChildren('link[type="text/css"]').length, // The number of files
 			loaded:0, // The counter of loaded files.
 			text:''
@@ -39,128 +37,124 @@
 		
 		// Get inline CSS.
 		$$('style').each(function(style){
-			this.input.text+=style.get('html');
-		}.bind(this));
+			input.text+=style.get('html');
+		});
 		// Get external CSS files.
 		$(document.head).getChildren('link[type="text/css"]').each(function(link){
 			new Request({
 				url:link.get('href'),
 				onSuccess:function(response){
-					this.input.text+=response;
-					this.input.loaded++;
-					if(this.input.loaded===this.input.count){
-						this.parse(this.input.text);
+					input.text+=response;
+					input.loaded++;
+					if(input.loaded===input.count){
+						parse(input.text);
 					}
-				}.bind(this)
+				}
 			}).send();
-		},this);
-	}.bind(this);
+		});
+	};
 	
 	// Wait until all CSS loads.
-	window.addEvent('domready',this.load,this);
+	window.addEvent('domready',load);
 	
 	// Parse for Bolster keywords.
-	this.parse=function(css){
+	var parse=function(css){
 		// Parse for CSS declarations.
 		css=css.split(/[{}]/g);
 		css.pop(); // Remove last item; will be the 'empty' text after the last '}'.
-		this.rules=[];
-		for(this.temp.i=0;this.temp.i<css.length/2;this.temp.i++){
-			this.rules.push({
-				selector:css[2*this.temp.i].trim().replace(/[\t\n]/g,''),
-				'css':css[2*this.temp.i+1],
+		var rules=[];
+		for(var i=0;i<css.length/2;i++){
+			rules.push({
+				selector:css[2*i].trim().replace(/[\t\n]/g,''),
+				'css':css[2*i+1],
 				declarations:[]
 			});
 		}
 		// Parse CSS into rules.
-		this.rules.each(function(rule){
-			this.temp.declarations=rule.css.split(/;/g);
-			this.temp.declarations.pop(); // Same as above.
-			this.temp.declarations.each(function(declaration){
+		rules.each(function(rule){
+			var declarations=rule.css.split(/;/g);
+			declarations.pop(); // Same as above.
+			declarations.each(function(declaration){
 				declaration=declaration.split(/:/g);
 				rule.declarations.push({
 					property:declaration[0].trim().replace(/[\t\n]/g,''),
 					value:declaration[1].trim().replace(/[\t\n]/g,'')
 				});
 			});
-		}.bind(this));
+		});
 		// Parse rules for Bolster properties.
-		this.rules.each(function(rule){
+		rules.each(function(rule){
 			rule.declarations.each(function(declaration){
-				this.get_effects().each(function(bolster_property){
+				get_effects().each(function(bolster_property){
 					if(declaration.property==='-bolster-'+bolster_property){
 						// Format whitespace.
-						this.temp.value=declaration.value.replace(/ +(?= )/g,''); // Remove multiple spaces.
-						this.temp.value=declaration.value.replace(/, /g,','); // Remove space after comma.
+						var value=declaration.value.replace(/ +(?= )/g,''); // Remove multiple spaces.
+						value=value.replace(/, /g,','); // Remove space after comma.
 						// Tokenize.
-						this.temp.args=[];
-						this.temp.last=0;
-						this.temp.quoted=false;
-						//escape=false;
-						this.temp.bucket=[];
-						for(this.temp.i=0,this.temp.len=this.temp.value.length;this.temp.i<this.temp.len;this.temp.i++){
-							switch(this.temp.value[this.temp.i]){
+						var args=[],last=0,quoted=false,bucket=[];
+						for(var i=0,len=value.length;i<len;i++){
+							switch(value[i]){
 								case "'":
 								case '"':
-									if(this.temp.quoted===false){
-										this.temp.quoted=this.temp.value[this.temp.i];
-										this.temp.last++; // So it won't match the leading quotation mark.
-									}else if(this.temp.quoted===this.temp.value[this.temp.i]){
-										this.temp.bucket.push(this.temp.value.substring(this.temp.last,this.temp.i));
-										this.temp.last=this.temp.i+1;
-										this.temp.quoted=false;
+									if(quoted===false){
+										quoted=value[i];
+										last++; // So it won't match the leading quotation mark.
+									}else if(quoted===value[i]){
+										bucket.push(value.substring(last,i));
+										last=i+1;
+										quoted=false;
 									}
 									break;
 								case ' ':
-									if(this.temp.quoted===false){
-										this.temp.bucket.push(this.temp.value.substring(this.temp.last,this.temp.i));
-										this.temp.last=this.temp.i+1;
+									if(quoted===false){
+										bucket.push(value.substring(last,i));
+										last=i+1;
 									}
 									break;
 								case ',':
-									if(this.temp.quoted===false){
-										this.temp.bucket.push(this.temp.value.substring(this.temp.last,this.temp.i));
-										this.temp.last=this.temp.i+1;
-										this.temp.args.push(this.temp.bucket);
-										this.temp.bucket=[];
+									if(quoted===false){
+										bucket.push(value.substring(last,i));
+										last=i+1;
+										args.push(bucket);
+										bucket=[];
 									}
 									break;
 							}
 						}
 						// The last token won't have a trailing comma or space.
-						this.temp.bucket.push(this.temp.value.substring(this.temp.last));
-						this.temp.args.push(this.temp.bucket);
+						bucket.push(value.substring(last));
+						args.push(bucket);
 						// Clean arguments.
-						this.temp.cleaned=[];
-						this.temp.args.each(function(tokens){
+						var cleaned=[];
+						args.each(function(tokens){
 							// Remove invalid tokens.
 							tokens=tokens.clean();
 							// Remove empty tokens.
-							this.temp.nonempty=[];
+							var nonempty=[];
 							tokens.each(function(token){
 								if(token!==''){
-									this.temp.nonempty.push(token);
+									nonempty.push(token);
 								}
-							}.bind(this));
-							this.temp.cleaned.push(this.temp.nonempty);
-						}.bind(this));
-						this.set_effect(rule.selector,bolster_property,this.temp.cleaned);
+							});
+							cleaned.push(nonempty);
+						});
+						set_effect(rule.selector,bolster_property,cleaned);
 					}
-				}.bind(this));
-			}.bind(this));
-		}.bind(this));
-	}.bind(this);
+				});
+			});
+		});
+	};
 	
 	// Get effect names.
-	this.get_effects=function(){
-		return Object.keys(this.bolsterfx);
+	var get_effects=function(){
+		return Object.keys(bolsterfx);
 	};
 	// Effect setter.
-	this.set_effect=function(selector,property,args){
-		this.bolsterfx[property](selector,args);
+	var set_effect=function(selector,property,args){
+		bolsterfx[property](selector,args);
 	};
 	// Effect configuration.
-	this.bolsterfx={
+	var bolsterfx={
 		// An example test property.
 		'test-property':function(selector,args){
 			console.log('Selector: '+selector+';');
@@ -191,7 +185,7 @@
 				a:args
 			});
 			if(args.indexOf('continuous')!==-1){
-				this.add_continuous(set_mount,{
+				add_continuous(set_mount,{
 					s:selector,
 					a:args
 				});
@@ -205,7 +199,7 @@
 					element.setStyle('height',element.getSize().y);
 				});
 				if(args[0].indexOf('continuous')!==-1){
-					this.add_continuous(function(s){
+					add_continuous(function(s){
 						$$(s).each(function(element){
 							element.setStyle('height','auto');
 							element.setStyle('height',element.getSize().y);
@@ -215,12 +209,12 @@
 			}
 			else{ // If they're an idiot and decide to use this for a regular height declaration.
 				sheets_width:
-				for(this.temp.i=0,this.temp.lenA=document.styleSheets.length;this.temp.i<this.temp.lenA;this.temp.i++){ // Traditional for loops must be used to use labels.
-					this.temp.sheet=document.styleSheets.length[this.temp.i];
-					for(this.temp.j=0,this.temp.lenB=this.temp.sheet.cssRules.length;this.temp.j<this.temp.lenB;this.temp.j++){
-						this.temp.rule=document.styleSheets[this.temp.i].cssRules[this.temp.j];
-						if(this.temp.rule.selectorText===selector){
-							this.temp.rule.style.setProperty('width',args[0][0]);
+				for(var i=0,lenA=document.styleSheets.length;i<lenA;i++){ // Traditional for loops must be used to use labels.
+					var sheet=document.styleSheets.length[i];
+					for(var j=0,lenB=sheet.cssRules.length;j<lenB;j++){
+						var rule=document.styleSheets[i].cssRules[j];
+						if(rule.selectorText===selector){
+							rule.style.setProperty('width',args[0][0]);
 							break sheets_width;
 						}
 					}
@@ -235,7 +229,7 @@
 					element.setStyle('width',element.getSize().x);
 				});
 				if(args[0].indexOf('continuous')!==-1){
-					this.add_continuous(function(s){
+					add_continuous(function(s){
 						$$(s).each(function(element){
 							element.setStyle('width','auto');
 							element.setStyle('width',element.getSize().x);
@@ -245,12 +239,12 @@
 			}
 			else{
 				sheets_width:
-				for(this.temp.i=0,this.temp.lenA=document.styleSheets.length;this.temp.i<this.temp.lenA;this.temp.i++){
-					this.temp.sheet=document.styleSheets.length[this.temp.i];
-					for(this.temp.j=0,this.temp.lenB=this.temp.sheet.cssRules.length;this.temp.j<this.temp.lenB;this.temp.j++){
-						this.temp.rule=document.styleSheets[this.temp.i].cssRules[this.temp.j];
-						if(this.temp.rule.selectorText===selector){
-							this.temp.rule.style.setProperty('width',args[0][0]);
+				for(var i=0,lenA=document.styleSheets.length;i<lenA;i++){
+					var sheet=document.styleSheets.length[i];
+					for(var j=0,lenB=sheet.cssRules.length;j<lenB;j++){
+						var rule=document.styleSheets[i].cssRules[j];
+						if(rule.selectorText===selector){
+							rule.style.setProperty('width',args[0][0]);
 							break sheets_width;
 						}
 					}
@@ -264,19 +258,19 @@
 	};
 	
 	// Continuous functions.
-	this.continuous=[];
-	this.add_continuous=function(func,param){
-		this.continuous.push({
+	var continuous=[];
+	var add_continuous=function(func,param){
+		continuous.push({
 			'function':func,
 			parameter:param
 		});
-	}.bind(this);
+	};
 	(function(){
-		this.continuous.each(function(item){
+		continuous.each(function(item){
 			item['function'](item.parameter);
 		});
-	}).bind(this).periodical(1/32);
+	}).periodical(1/32);
 	
 	// Rescan for new elements.
 	// Dammit, DOMSubtreeModified is deprecated?
-}).bind({})(window.MooTools);
+})(window.MooTools);
